f1b018bac0b86123f071d2c111e54ed4daee988c
Julian Montez <julian.montez@gmail.com>
2014-07-15 15:07:59 -0500

    Simplify role provisioning

--- taas/environment.py
+++ taas/environment.py
@@ -49,8 +49,9 @@ class Environment(object):
             self.users.append(user)
 
     def provision_role(self, user):
+        LOG.info('Provisioning user role')
         roles = self.keystone.roles.list()
-        role = next(role for role in roles if '_member_' in role.name)
+        role = [role for role in roles if '_member_' in role.name][0]
         try:
             self.keystone.roles.add_user_role(user, role, tenant=self.tenant)
         except Exception as exc:
